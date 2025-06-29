92987df60ce69b966b53b5f71d1037d1df7800d7
gumswordsman <gumswordsman@gmail.com>
2025-06-30 01:24:20 +1000

    Пися попа классно

--- scripts/Spells/SpellButton.cs
+++ scripts/Spells/SpellButton.cs
@@ -3,47 +3,47 @@ using System;
 
 public partial class SpellButton : TextureButton
 {
-    [Export]
-    NodePath cooldownBarPath;
-    [Export]
-    NodePath cooldownPath;
-    [Export]
-    NodePath keyPath;
+	[Export]
+	NodePath cooldownBarPath;
+	[Export]
+	NodePath cooldownPath;
+	[Export]
+	NodePath keyPath;
 
-    private ProgressBar cooldownBarNode;
-    private Label keyNode;
-    private Label cooldownNode;
+	private ProgressBar cooldownBarNode;
+	private Label keyNode;
+	private Label cooldownNode;
 
-    private void GetNodesByPath()
-    {
-        cooldownBarNode = GetNode<ProgressBar>(cooldownBarPath);
-        cooldownNode = GetNode<Label>(cooldownPath);
-        keyNode = GetNode<Label>(keyPath);
-    }
+	private void GetNodesByPath()
+	{
+		cooldownBarNode = GetNode<ProgressBar>(cooldownBarPath);
+		cooldownNode = GetNode<Label>(cooldownPath);
+		keyNode = GetNode<Label>(keyPath);
+	}
 
-    public override void _Ready()
-    {
-        GetNodesByPath();
-        changeKey = "Q";
-    }
+	public override void _Ready()
+	{
+		GetNodesByPath();
+		changeKey = "Q";
+	}
 
-    public String changeKey
-    {
-        set
-        {
-            changeKey = value;
-            keyNode.Text = value;
+	public String changeKey
+	{
+		set
+		{
+			changeKey = value;
+			keyNode.Text = value;
 
-            Shortcut = new Shortcut();
+			Shortcut = new Shortcut();
 
-            var inputKey = new InputEventKey();
+			var inputKey = new InputEventKey();
 
-            var rune = System.Text.Rune.GetRuneAt(value, 0);
-            inputKey.Keycode = (Key)rune.Value;
+			var rune = System.Text.Rune.GetRuneAt(value, 0);
+			inputKey.Keycode = (Key)rune.Value;
 
-            Shortcut.Events = [inputKey];
-        }
-    }
+			Shortcut.Events = [inputKey];
+		}
+	}
 
 
 
